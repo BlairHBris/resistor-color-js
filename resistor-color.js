d6f8cc271faae765b47b544de327f172e5642948
Blair Bristow <blairhbristow@gmail.com>
2022-04-25 15:16:10 -0400

    Add solution

--- resistor-color.js
+++ resistor-color.js
@@ -1,3 +1,20 @@
-export function colorCode() {}
+/* eslint-disable prettier/prettier */
+const colors = [
+    {name:'black', id:0},
+    {name:'brown', id:1},
+    {name:'red', id:2},
+    {name:'orange', id:3},
+    {name:'yellow', id:4},
+    {name:'green', id:5},
+    {name:'blue', id:6},
+    {name:'violet', id:7},
+    {name:'grey', id:8},
+    {name:'white', id:9}
+]
 
-export const COLORS = [];
+export function colorCode(colorString) {
+    const targetColor = colors.find(color => color.name == colorString)
+    return targetColor.id
+}
+
+export const COLORS = colors.map(color => color.name)
